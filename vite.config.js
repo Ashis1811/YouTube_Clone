e6e942c5d8c37103d986c67b4219c4c7bef5160e
Ashis1811 <ashisshaw1811@gmail.com>
2025-06-21 16:33:15 +0530

    "first commit"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/YouTube_Clone/',
+export default defineConfig({ 
   plugins: [react()],
 })
