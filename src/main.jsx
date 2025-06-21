d360e05b384f2c6451d97c54c555baf54ce246e7
Ashis1811 <ashisshaw1811@gmail.com>
2025-06-21 16:41:15 +0530

    "first commit"

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/YouTube_Clone"><App /></BrowserRouter>
+    <BrowserRouter ><App /></BrowserRouter>
 
   </React.StrictMode>,
 )
